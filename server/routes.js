643b9d3dbf7253766b8b479fc519a858db0d912d
Katarzyna Kucharczyk <katarzyna.kucharczyk@polidea.com>
2015-11-06 01:17:10 +0100

    proper jquery post

--- server/routes.js
+++ server/routes.js
@@ -9,11 +9,23 @@ var transpile = require('./transpilers.js').transpile;
 
 app.use(jsonParser);
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+
 app.post('/parse/:parser', function (request, response) {
   var parser = request.params.parser;
 
+  debugger;
   if (!request.body) return response.sendStatus(400);
 
+console.log("code " + request.body.code);
+console.log("code " + request.body);
   if (parser) {
     try {
       var transpileFun = transpile[parser];
